bf6a7a83f0acee10f38231b7f2188ffefcd7e32d
Sanskar2709 <sanskartiwari270901@gmail.com>
2024-12-19 04:34:37 +0530

    hghg

---
+++ backend1/dist/messages.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.JOIN_ROOM = exports.OPPONENT_DISCONNECTED = exports.JOIN_GAME = exports.GAME_OVER = exports.MOVE = exports.INIT_GAME = void 0;
+exports.INIT_GAME = "init_game";
+exports.MOVE = 'move';
+exports.GAME_OVER = "game_over";
+exports.JOIN_GAME = 'join_game';
+exports.OPPONENT_DISCONNECTED = 'opponent_disconnected';
+exports.JOIN_ROOM = 'join_room';
---
+++ backend1/src/messages.ts
@@ -0,0 +1,6 @@
+export const INIT_GAME = "init_game";
+export const MOVE = 'move';
+export const GAME_OVER = "game_over";
+export const JOIN_GAME = 'join_game';
+export const OPPONENT_DISCONNECTED = 'opponent_disconnected';
+export const JOIN_ROOM = 'join_room';
